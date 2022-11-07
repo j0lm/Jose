6293f8df0b401752fe87d9cbe18ee8415a6d2170
j0lm <jose0lmedojr@gmail.com>
2022-11-06 23:48:22 -0600

    initial commit

---
+++ fe/src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import "../styles.css"
+
+class Navbar extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div>
+                <h1>hasdasdasdi</h1>
+            </div>
+        );
+    }
+}
+ 
+export default Navbar;
