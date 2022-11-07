0ceb9ab3f7754d12237a4352ad0a7a043ace240e
j0lm <jose0lmedojr@gmail.com>
2022-11-07 02:32:44 -0600

    finished navbar

---
+++ fe/src/components/navbar/MenuItems.jsx
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cn: 'nav-links'
+    }, 
+    {
+        title: 'Projects',
+        url: '#',
+        cn: 'nav-links'
+    }, 
+    {
+        title: 'Contact Me',
+        url: '#',
+        cn: 'nav-links'
+    }, 
+]
--- fe/src/components/navbar/Navbar.jsx
+++ fe/src/components/navbar/Navbar.jsx
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import { MenuItems } from './MenuItems'
 import "../styles.css"
 
 class Navbar extends Component {
-    state = {  } 
+    state = { 
+        clicked: false
+    } 
+
+    handleMenuClick = () => {
+        this.setState({clicked: !(this.state.clicked)});
+    }
+
     render() { 
         return (
-            <div>
-                <h1>hasdasdasdi</h1>
-            </div>
+            <nav className="NavbarItems">
+                <h1 className='navbar-logo'><i className="fa-solid fa-terminal"></i></h1>
+                <div 
+                    className="menu-icon" 
+                    onClick={this.handleMenuClick}>
+                    <i className={this.state.clicked ? "fa-solid fa-xmark" : "fa-solid fa-bars"}></i>
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                    { MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cn} href={item.url}>
+                                    {item.title}
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
         );
     }
 }
