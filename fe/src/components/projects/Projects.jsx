9fc64e19c938a16302499e1d92be7ee107fcbfea
j0lm <jose0lmedojr@gmail.com>
2022-11-08 13:21:31 -0600

    project dir

---
+++ fe/src/components/projects/Projects.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import 'projects.css'
+
+function Projects() {
+    return ( <div className='Projects'>
+        <h1>Projects</h1>
+    </div> );
+}
+
+export default Projects;
